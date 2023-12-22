395486cebd7494227b466e6d93caad999cf31bf9
Abhishek Kumar <31275711+kumar4895abhishek@users.noreply.github.com>
2023-12-22 21:25:22 +0530

    UserService Created login,logout,Auth

---
+++ src/main/java/com/example/userservice/services/RoleService.java
@@ -0,0 +1,21 @@
+package com.example.userservice.services;
+
+import com.example.userservice.repositories.RoleRepository;
+import com.example.userservice.models.Role;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+    private RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public Role createRole(String name) {
+        Role role = new Role();
+        role.setRole(name);
+
+        return roleRepository.save(role);
+    }
+}
