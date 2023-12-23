0dd55996b4f4093868757a67549ccda25e376559
Abhishek Kumar <31275711+kumar4895abhishek@users.noreply.github.com>
2023-12-23 11:14:31 +0530

    Your commit message here

--- src/main/java/com/example/userservice/services/AuthService.java
+++ src/main/java/com/example/userservice/services/AuthService.java
@@ -6,7 +6,10 @@ import com.example.userservice.repositories.UserRepository;
 import com.example.userservice.dtos.UserDto;
 import com.example.userservice.models.SessionStatus;
 import com.example.userservice.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMapAdapter;
 
-import java.util.HashMap;
-import java.util.Optional;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 @Service
 public class AuthService {
@@ -37,20 +41,47 @@ public class AuthService {
 
         if (userOptional.isEmpty()) {
             return null;
-        }
+        } //return exception
 
         User user = userOptional.get();
 
         if (!bCryptPasswordEncoder.matches(password,user.getPassword())) {
-            return null;
+            throw new RuntimeException("Wrong Password Entered"); // throw custom exception
         }
 
-        String token = RandomStringUtils.randomAlphanumeric(30);
+       // String token = RandomStringUtils.randomAlphanumeric(30); // JWT use
+        // Create a test key suitable for the desired HMAC-SHA algorithm:
+        MacAlgorithm alg = Jwts.SIG.HS256; //or HS384 or HS256
+        SecretKey key = alg.key().build();
+
+//        String message = "{\n" +
+//                "\"email\": \"abhishek\",\n" +
+//                "\"role\":[\"ADMIN\",\"USER\"]}";
+        Map<String, Object> jsonMap=new HashMap<>();
+        jsonMap.put("email",user.getEmail());
+        jsonMap.put("roles", List.of(user.getRoles()));
+        jsonMap.put("createdAt",new Date());
+        jsonMap.put("expiryAt", DateUtils.addDays(new Date(),5));
+
+       // byte[] content = message.getBytes(StandardCharsets.UTF_8);
+
+        // Create the compact JWS: JSON WEB SIGNATURE
+      //  String jws = Jwts.builder().content(content, "text/plain").signWith(key, alg).compact();
+
+        String jws=Jwts.builder()
+                .claims(jsonMap)
+                .signWith(key,alg)
+                .compact();
+        // Parse the compact JWS:
+       // content = Jwts.parser().verifyWith(key).build().parseSignedContent(jws).getPayload();
+
+       // assert message.equals(new String(content, StandardCharsets.UTF_8));
 
         Session session = new Session();
         session.setSessionStatus(SessionStatus.ACTIVE);
-        session.setToken(token);
+        session.setToken(jws);
         session.setUser(user);
+        //session.setExpiringAt(); // for expiry set
         sessionRepository.save(session);
 
         UserDto userDto = new UserDto();
@@ -60,7 +91,7 @@ public class AuthService {
 //        headers.put(HttpHeaders.SET_COOKIE, token);
 
         MultiValueMapAdapter<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
-        headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
+        headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + jws);
 
 
 
